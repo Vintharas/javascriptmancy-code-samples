f4abaf2661a81ffcfad25d157e08f17da683c4d9
Jaime González García <jaime.gg83@gmail.com>
2017-08-16 17:52:42 +0200

    Added example of importing declarations for third parties

---
+++ 03-oop/typescript/third-parties.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var ramda_1 = require("ramda");
+var add5 = ramda_1.add(5);
+console.log("5 + 5: " + add5(5));
+console.log("5 + 10: " + add5(10));
---
+++ 03-oop/typescript/third-parties.ts
@@ -0,0 +1,6 @@
+import { add } from 'ramda';
+
+const add5 = add(5);
+
+console.log(`5 + 5: ${add5(5)}`);
+console.log(`5 + 10: ${add5(1)}`);
